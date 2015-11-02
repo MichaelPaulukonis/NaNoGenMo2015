f35678d511fe6500f8528b3eea415f061c980a98
Michael Paulukonis <xraysmalevich@gmail.com>
2015-11-01 23:32:14 -0500

    heartless notes

---
+++ heartless/README.md
@@ -0,0 +1,2 @@
+## Implementation of n-nested helper tale
+See _The Giant with no Heart in his body_ or _Koschei the Deathless_
---
+++ heartless/index.js
@@ -0,0 +1,24 @@
+/*
+ So. Plan of action
+ THESE ARE ALL TO START THINGS OFF
+ Main body of story is static
+ call to function to get n-helpers
+ helper includes helper, hero encounter-locale,
+ helper ability, helper antagonist, antagonist method of defeat
+ which may just come from ability, or should relate to it
+ to begin, these will be stupidly static, with incremented numbers.
+ concentrate on getting a dumb skeleton
+ and, hey, maybe some tests, because that always trips you up
+ when you least expect it and gee these don't really _need_ test do they?
+ anyhow.
+ Getting this "working" should help solidify the ideas of what properties
+ the helper "object" should have.
+ Then look into auto-generation of helpers and associated landscape
+ the latter is an issu,e because we probably don't want 11 helpers in a row
+ that are involved with crossing rivers or adjacent bodies of water.
+ or maybe that's a secondary concern....
+ I'm _thinking_ that some sort of knoweldge/rule system is required
+ to deal with abilities, locales, antagonists, etc.
+ but maybe it doesn't have to me even that smart.
+ see 2014's example of .... an imaginary bestiary. that I can't find back.
+*/
