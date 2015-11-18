c216202093099be8c574652a0e8562b956a96d3f
Michael Paulukonis <xraysmalevich@gmail.com>
2015-11-17 22:14:42 -0500

    better narrative, plus offline data-reading. notes

--- github-narrative/index.js
+++ github-narrative/index.js
@@ -1,107 +1,159 @@
-var request = require('sync-request'),
-    parse = require('parse-link-header'),
-    config = require('./config');
+var narrative = function() {
 
-// temporary hard-coded username
-// https://developer.github.com/v3/activity/events/
-// can only grab last 90 days of events/300 items
-// whichever is less
-// var url = 'https://api.github.com/users/MichaelPaulukonis/events';
-var url = 'https://api.github.com/repos/dariusk/NaNoGenMo-2015/issues';
+  var request = require('sync-request'),
+      parse = require('parse-link-header'),
+      config = require('./config');
 
-var extend = function(obj, src) {
-  for (var key in src) {
-    console.log('key', key);
-    if (src.hasOwnProperty(key)) {
-      console.log('yes!');
-      obj[key] = src[key];
+  // temporary hard-coded username
+  // https://developer.github.com/v3/activity/events/
+  // can only grab last 90 days of events/300 items
+  // whichever is less
+  // var url = 'https://api.github.com/users/MichaelPaulukonis/events';
+  var url = 'https://api.github.com/repos/dariusk/NaNoGenMo-2015/issues';
+
+
+  // let's do this syncronously becuase... :::sigh:::
+  // quicker ?
+  var getpage = function(url) {
+
+    var res = request('GET', url,
+                      { headers: { 'user-agent': 'node.js',
+                                   'access_token': config.access_token }});
+    var parsed = parse(res.headers.link);
+
+    return { body: JSON.parse(res.getBody('utf8')), links: parsed };
+
+  };
+
+  var next = url,
+      issues = [];
+
+  if (config.use_local) {
+    issues = require(config.local_file);
+  } else {
+
+    while (true) {
+      // console.log('url: ', next);
+      var page = getpage(next);
+
+      // console.log('page.body: ', page.body);
+
+      issues = issues.concat(page.body); // extend(issues, page.body);
+
+      if(!page.links.next) { break; }
+      next = page.links.next.url;
+
     }
-  }
-  return obj;
-};
-
-// let's do this syncronously becuase... :::sigh:::
-// quicker ?
-var getpage = function(url) {
-
-  var res = request('GET', url,
-                    { headers: { 'user-agent': 'node.js',
-                                 'access_token': config.access_token }});
-  var parsed = parse(res.headers.link);
-
-  return { body: JSON.parse(res.getBody('utf8')), links: parsed };
-
-};
-
-var next = url,
-    issues = [];
-
-while (true) {
-  // console.log('url: ', next);
-  var page = getpage(next);
-
-  // console.log('page.body: ', page.body);
-
-  issues = issues.concat(page.body); // extend(issues, page.body);
-
-  if(!page.links.next) { break; }
-  next = page.links.next.url;
-
-}
-
-// This is a timestamp in ISO 8601 format: YYYY-MM-DDTHH:MM:SSZ.
-// '1445861152000'
-// > new Date('2015-10-26T12:05:52Z').toString()
-// 'Mon Oct 26 2015 08:05:52 GMT-0400 (Eastern Daylight Time)'
-// console.log('here are ALL of the issues: \n\n', issues);
-
-var narrate = function(issues) {
-
-  var txt = [];
-
-  for (var i = 0; i < issues.length; i++) {
-
-    var issue = issues[i],
-        openDate = new Date(issue.created_at).toString().replace(/ GMT.*/, '');
-    name = issue.user.login,
-    title = issue.title;
-
-    // some variations and comments on notable things
-    // morning, afternoon, evening, middle-of-the-night (which will not be accurate, but whatevs)
-    txt.push(`On ${openDate}, ${name} opened a new issue called "${title}".`);
 
   }
+  // This is a timestamp in ISO 8601 format: YYYY-MM-DDTHH:MM:SSZ.
+  // > new Date('2015-10-26T12:05:52Z').toString()
+  // 'Mon Oct 26 2015 08:05:52 GMT-0400 (Eastern Daylight Time)'
 
-  return txt.join('\n');
 
-};
+  var isAdmin = function(issue) {
+    var isAdmin = false;
+
+    var labels = (issue.labels !== undefined ? issue.labels : []);
+
+    for (var i = 0; i < labels.length; i++) {
+      if (labels[i].name === 'admin') {
+        isAdmin = true;
+        break;
+      }
+    }
+
+    return isAdmin;
+  };
+
+  var isCompleted = function(issue) {
+    var isCompleted = false;
+
+    var labels = (issue.labels !== undefined ? issue.labels : []);
+
+    for (var i = 0; i < labels.length; i++) {
+      if (labels[i].name === 'completed') {
+        isCompleted = true;
+        break;
+      }
+    }
+
+    return isCompleted;
+  };
 
 
-// console.log(JSON.stringify(issues, null, 2));
+  var narrate = function(issues) {
 
-var text = narrate(issues);
+    var txt = [];
 
-console.log(text);
+    // TODO: get a sorted list
+    // TODO: create Event objects
+    // those will be sorted next
+    // Events will have properties like notability or interest, or summat
 
-// TODO: optionally parse a local copy of file
+    issues.sort(function(a,b) {
+      // a = new Date(a.created_at);
+      // b = new Date(b.created_at);
+      // return a > b ? -1 : a < b ? 1 : 0;
+      return new Date(a.created_at) - new Date(b.created_at);
+    });
 
-// actually, this looks really good, too:
-// https://www.npmjs.com/package/github-scraper
+    for (var i = 0; i < issues.length; i++) {
 
-// [...]
-//      { url: '/dariusk/NaNoGenMo-2015/issues/137',
-//        title: 'Automatic Essay Grading + (Markov Chains | Genetic Algorithm) = Novel?',
-//        created: '2015-11-05T15:23:39Z',
-//        author: 'nicholasg3',
-//        comments: 1,
-//        labels: [] },
-//      { url: '/dariusk/NaNoGenMo-2015/issues/136',
-//        title: 'The TPP: A "Found" Generated Novel',
-//        created: '2015-11-05T14:24:46Z',
-//        author: 'coleww',
-//        comments: 6,
-//        labels: [Object] } ],
-//   url: 'https://github.com/dariusk/nanogenmo-2015/issues',
-//   open: 159,
-//   closed: 0,
-//   next_page: '/dariusk/nanogenmo-2015/issues?page=2&q=is%3Aissue+is%3Aopen' }
+      var issue = issues[i],
+          openDate = new Date(issue.created_at).toString().replace(/ GMT.*/, ''),
+          name = issue.user.login,
+          title = issue.title;
+
+      // some variations and comments on notable things
+      // morning, afternoon, evening, middle-of-the-night (which will not be accurate, but whatevs)
+
+      var msg = [`On ${openDate}, ${name} opened a new issue called "${title}".`];
+
+      // this is crude, horrible, and also crude. PROOF OF CONCEPT, OK?
+      if (isAdmin(issue)) {
+        msg.push('But it\'s an admin issue, so who cares?');
+      } else if (isCompleted(issue)) {
+        msg.push('And it\'s been completed. Sweet!');
+      }
+
+      txt.push(msg.join(' '));
+9
+    }
+
+    return txt.join('\n');
+
+  };
+
+
+  // console.log(JSON.stringify(issues, null, 2));
+
+  var text = narrate(issues);
+
+  console.log(text);
+
+  // TODO: optionally parse a local copy of file
+
+  // actually, this looks really good, too:
+  // https://www.npmjs.com/package/github-scraper
+
+  // [...]
+  //      { url: '/dariusk/NaNoGenMo-2015/issues/137',
+  //        title: 'Automatic Essay Grading + (Markov Chains | Genetic Algorithm) = Novel?',
+  //        created: '2015-11-05T15:23:39Z',
+  //        author: 'nicholasg3',
+  //        comments: 1,
+  //        labels: [] },
+  //      { url: '/dariusk/NaNoGenMo-2015/issues/136',
+  //        title: 'The TPP: A "Found" Generated Novel',
+  //        created: '2015-11-05T14:24:46Z',
+  //        author: 'coleww',
+  //        comments: 6,
+  //        labels: [Object] } ],
+  //   url: 'https://github.com/dariusk/nanogenmo-2015/issues',
+  //   open: 159,
+  //   closed: 0,
+  //   next_page: '/dariusk/nanogenmo-2015/issues?page=2&q=is%3Aissue+is%3Aopen' }
+
+
+}();
